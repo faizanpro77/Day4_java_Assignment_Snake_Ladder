d94b96c07be6cac1ed5aae9d17b0c15d5695e51c
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-21 21:08:30 +0530

    ADDED: starting position of player and print

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -2,7 +2,10 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        System.out.println("Welcom  to snake & ladder game");
-    }
 
+        System.out.println("Welcom  to snake & ladder game");
+        //Starting position of player
+        int playerPosition = 0;
+        System.out.println("Starting position of player : "+playerPosition);
+    }
 }
