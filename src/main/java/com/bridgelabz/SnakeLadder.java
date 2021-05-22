53bb44ce1461b56bbf5fd61ce9388eee747ff22b
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-22 19:04:32 +0530

    ADDED: fuctionality to repeat till the Player reaches the winning position 100

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -3,34 +3,36 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static final int ladder = 1;
     public static final int snake = 2;
-    public static final int winning_points = 100;
+    public static final int winningPoints = 100;
 
     public static void main(String[] args) {
-        System.out.println("Welcom  to snake & ladder game");
+        System.out.println("Welcome  to snake & ladder game");
         //Starting position of player
         int  playerPosition = 0;
+        int count = 1;
         System.out.println("Starting position of player : "+playerPosition);
-        //To print random variables between 1-6 as dies
-        int random = (int)(Math.random()*6) + 1;
-        System.out.println("Rolls Dies : "+random);
-        //Random values to set ladder and snake
-        int check = (int) (Math.random()*3);
-        int result=0;
-        //To check ladder or snake
-        switch ( check ){
-            case ladder:
-                System.out.println("Ladder");
-                result += random;
-                break;
-            case snake:
-                System.out.println("Snake");
-                result -= random;
-                break;
-            default:
-                System.out.println("Not play");
-                result = 0;
+        for(int dice = 1; playerPosition < winningPoints; dice++) {
+            //To print random variables between 1-6 as dies
+            int random = (int) (Math.random() * 6) + 1;
+            //Random values to set ladder and snake
+            int check = (int) (Math.random() * 3);
+            int result = 0;
+            //To check ladder or snake
+            switch (check) {
+                case ladder:
+                    result += random;
+                    break;
+                case snake:
+                    result -= random;
+                    break;
+                default:
+                    result = 0;
+            }
+            playerPosition += result;
+            if(playerPosition < 0)
+                playerPosition = 0;
+            count++;
         }
-        playerPosition += result;
-        System.out.println("Posotoin of player : "+playerPosition);
+        System.out.println("Posotoin of player : " + playerPosition + " Winner" + "\n" + "number of time dice rolled : " + count);
     }
 }
