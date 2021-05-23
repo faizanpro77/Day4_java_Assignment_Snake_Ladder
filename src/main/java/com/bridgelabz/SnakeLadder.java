2d367844746de6f47ba5317acaab18c755fbd6fa
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-23 10:09:28 +0530

    REFACTORED: to snake and ladder with  switch case and remove variable

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,38 +1,52 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public static final int ladder = 1;
-    public static final int snake = 2;
+    public static final int ladder1 = 5;
+    public static final int ladder2 = 9;
+    public static final int ladder3 = 20;
+    public static final int snake1 = 40;
+    public static final int snake2 = 53;
+    public static final int snake3 = 61;
     public static final int winningPoints = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome  to snake & ladder game");
         //Starting position of player
         int  playerPosition = 0;
-        int count = 1;
         System.out.println("Starting position of player : "+playerPosition);
         for(int dice = 1; playerPosition < winningPoints; dice++) {
             //To print random variables between 1-6 as dies
             int random = (int) (Math.random() * 6) + 1;
             //Random values to set ladder and snake
-            int check = (int) (Math.random() * 3);
+            int check = (int) (Math.random() * 65);
             int result = 0;
             //To check ladder or snake
             switch (check) {
-                case ladder:
-                    result += random;
+                case ladder1:
+                    result += 48;
                     break;
-                case snake:
-                    result -= random;
+                case ladder2:
+                    result += 30;
+                    break;
+                case ladder3:
+                    result += 42;
+                    break;
+                case snake1:
+                    result -= 3;
+                    break;
+                case snake2:
+                    result -= 10;
+                    break;
+                case snake3:
+                    result -= 17;
                     break;
                 default:
-                    result = 0;
+                    result = random;
             }
             playerPosition += result;
             if(playerPosition < 0)
                 playerPosition = 0;
-            count++;
         }
-        System.out.println("Posotoin of player : " + playerPosition + " Winner" + "\n" + "number of time dice rolled : " + count);
+        System.out.println("Position of player : " + playerPosition + " Winner");
     }
 }
