23090c595c01503c266cc0ddbe58d21cdf45c0d9
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-23 10:43:50 +0530

    REFACTORED: to snake and ladder with  switch case

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,36 +1,52 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public static final int ladder = 1;
-    public static final int snake = 2;
-    public static final int winning_points = 100;
+    public static final int ladder1 = 5;
+    public static final int ladder2 = 9;
+    public static final int ladder3 = 20;
+    public static final int snake1 = 40;
+    public static final int snake2 = 53;
+    public static final int snake3 = 61;
+    public static final int notPlay = 0;
 
     public static void main(String[] args) {
-        System.out.println("Welcom  to snake & ladder game");
+        System.out.println("Welcome  to snake & ladder game");
         //Starting position of player
         int  playerPosition = 0;
         System.out.println("Starting position of player : "+playerPosition);
-        //To print random variables between 1-6 as dies
-        int random = (int)(Math.random()*6) + 1;
-        System.out.println("Rolls Dies : "+random);
-        //Random values to set ladder and snake
-        int check = (int) (Math.random()*3);
-        int result=0;
-        //To check ladder or snake
-        switch ( check ){
-            case ladder:
-                System.out.println("Ladder");
-                result += random;
-                break;
-            case snake:
-                System.out.println("Snake");
-                result -= random;
-                break;
-            default:
-                System.out.println("Not play");
-                result = 0;
-        }
-        playerPosition += result;
-        System.out.println("Posotoin of player : "+playerPosition);
+            //To print random variables between 1-6 as dies
+            int random = (int) (Math.random() * 6) + 1;
+            //Random values to set ladder and snake
+            int check = (int) (Math.random() * 65);
+            int result = 0;
+            //To check ladder or snake
+            switch (check) {
+                case ladder1:
+                    result += 48;
+                    break;
+                case ladder2:
+                    result += 30;
+                    break;
+                case ladder3:
+                    result += 42;
+                    break;
+                case snake1:
+                    result -= 3;
+                    break;
+                case snake2:
+                    result -= 10;
+                    break;
+                case snake3:
+                    result -= 17;
+                    break;
+                case notPlay:
+                    System.out.println("not play");
+                    result = 0;
+                    break;
+                default:
+                    result = random;
+            }
+            playerPosition += result;
+            System.out.println("Position of player : " + playerPosition);
     }
 }
