f5d7322b9b490709594712082113c0f983e1e7c4
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-26 15:31:33 +0530

    MODIFIED: change switch condition

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,13 +1,9 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public static final int notPlay = 0;
-    public static final int ladder1 = 5;
-    public static final int ladder2 = 9;
-    public static final int ladder3 = 20;
-    public static final int snake1 = 40;
-    public static final int snake2 = 53;
-    public static final int snake3 = 61;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int NOT_PLAY = 3;
     public static final int winningPoints = 100;
 
     public static void main(String[] args) {
@@ -17,41 +13,28 @@ public class SnakeLadder {
         System.out.println("Starting position of player : "+playerPosition);
         for(int dice = 1; playerPosition < winningPoints; dice++) {
             //To print random variables between 1-6 as dies
-            int random = (int) (Math.random() * 6) + 1;
+            int die = (int) (Math.random() * 6) + 1;
             //Random values to set ladder and snake
-            int check = (int) (Math.random() * 65);
+            int check = (int) (Math.random() * 3) + 1;
             int result = 0;
             //To check ladder or snake
             switch (check) {
-                case ladder1:
-                    result += 48;
+                case LADDER:
+                    result += die;
                     break;
-                case ladder2:
-                    result += 30;
+                case SNAKE:
+                    result -= die;
                     break;
-                case ladder3:
-                    result += 42;
-                    break;
-                case snake1:
-                    result -= 3;
-                    break;
-                case snake2:
-                    result -= 10;
-                    break;
-                case snake3:
-                    result -= 17;
-                    break;
-                case notPlay:
+                case NOT_PLAY:
                     System.out.println("not play");
                     result = 0;
                     break;
-                default:
-                    result = random;
             }
             playerPosition += result;
+            //If player position below 0 then player position is 0
             if(playerPosition < 0)
                 playerPosition = 0;
         }
-        System.out.println("Position of player : " + playerPosition + " Winner");
+        System.out.println("Position of player : " + playerPosition + " winner");
     }
 }
