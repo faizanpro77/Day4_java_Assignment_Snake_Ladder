8abc9197bf3eb136ec5924e338e5f031a65972e8
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-26 14:57:58 +0530

    REFACTORED: change variable name

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,52 +1,34 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public static final int ladder1 = 5;
-    public static final int ladder2 = 9;
-    public static final int ladder3 = 20;
-    public static final int snake1 = 40;
-    public static final int snake2 = 53;
-    public static final int snake3 = 61;
-    public static final int notPlay = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int NOT_PLAY = 3;
 
     public static void main(String[] args) {
         System.out.println("Welcome  to snake & ladder game");
         //Starting position of player
         int  playerPosition = 0;
         System.out.println("Starting position of player : "+playerPosition);
-            //To print random variables between 1-6 as dies
-            int random = (int) (Math.random() * 6) + 1;
-            //Random values to set ladder and snake
-            int check = (int) (Math.random() * 65);
-            int result = 0;
-            //To check ladder or snake
-            switch (check) {
-                case ladder1:
-                    result += 48;
-                    break;
-                case ladder2:
-                    result += 30;
-                    break;
-                case ladder3:
-                    result += 42;
-                    break;
-                case snake1:
-                    result -= 3;
-                    break;
-                case snake2:
-                    result -= 10;
-                    break;
-                case snake3:
-                    result -= 17;
-                    break;
-                case notPlay:
-                    System.out.println("not play");
-                    result = 0;
-                    break;
-                default:
-                    result = random;
-            }
-            playerPosition += result;
-            System.out.println("Position of player : " + playerPosition);
+        //To print random variables between 1-6 as dies
+        int die = (int) (Math.random() * 6) + 1;
+        //Random values to set ladder and snake
+        int check = (int) (Math.random() * 3) + 1;
+        int result = 0;
+        //To check ladder or snake
+        switch (check) {
+            case LADDER:
+            result += die;
+            break;
+            case SNAKE:
+            result -= die;
+            break;
+            case NOT_PLAY:
+        System.out.println("not play");
+        result = 0;
+        break;
+        }
+        playerPosition += result;
+        System.out.println("Position of player : " + playerPosition);
     }
 }
