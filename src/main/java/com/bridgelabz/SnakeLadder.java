df17d03f1afc87bdb272939307923a69f21843de
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-26 18:29:07 +0530

    MODIFIED: add functionality to play with two player

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,41 +1,78 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
-    public static final int LADDER = 1;
-    public static final int SNAKE = 2;
-    public static final int NOT_PLAY = 3;
-    public static final int WINNING_POSITION = 100;
+    public static final int LADDER = 0;
+    public static final int SNAKE = 1;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome  to snake & ladder game");
-        //Starting position of player
-        int  playerPosition = 0;
-        System.out.println("Starting position of player : "+playerPosition);
-        for(int dice = 1; playerPosition < WINNING_POSITION; dice++) {
-            //To print random variables between 1-6 as dies
-            int die = (int) (Math.random() * 6) + 1;
-            //Random values to set ladder and snake
-            int check = (int) (Math.random() * 3) + 1;
-            int result = 0;
-            //To check ladder or snake
-            switch (check) {
+    static void dieRollPosition() {
+        int player1=0;
+        int player2=0;
+        int rolls=1;
+        while(player1<100 && player2<100) {
+            int dice1=(int) Math.floor(Math.random()*6+1);
+            int option=(int) Math.floor(Math.random()*2);
+            int temp1=player1;
+            int temp2=player2;
+            int dice2;
+            switch(option) {
                 case LADDER:
-                    result += die;
+                    //Ladder-play
+                    if(rolls%2!=0) {
+                        player1=player1+dice1;
+                        System.out.println("player1 position="+player1);
+                        dice2=(int) Math.floor(Math.random()*6+1);
+                        player1=player1+dice2;
+                        System.out.println("player1 position="+player1);
+                    }else {
+                        player2=player2+dice1;
+                        System.out.println("player2 position="+player2);
+                        dice2=(int) Math.floor(Math.random()*6+1);
+                        player2=player2+dice2;
+                        System.out.println("player2 position="+player2);
+                    }
                     break;
                 case SNAKE:
-                    result -= die;
+                    //Snake-play
+                    if(rolls%2!=0) {
+                        player1=player1-dice1;
+                        System.out.println("player1 position="+player1);
+                    }else {
+                        player2=player2-dice1;
+                        System.out.println("player2 position="+player2);
+                    }
                     break;
-                case NOT_PLAY:
-                    System.out.println("not play");
-                    result = 0;
+                default:
+                    //no-play
                     break;
             }
-            playerPosition += result;
-            //If player position below 0 then player position is 0
-            if(playerPosition < 0)
-                playerPosition = 0;
+            if(rolls%2!=0) {
+                if(player1<0) {
+                    player1=0;
+                }
+                if(player1>100) {
+                    player1=temp1;
+                }
+            }else {
+                if(player2<0) {
+                    player2=0;
+                }
+                if(player2>100) {
+                    player2=temp2;
+                }
+            }
+            rolls++;
         }
-        System.out.println("Position of player : " + playerPosition + " winner");
+        System.out.println("Number of rolls required to win the game="+(rolls-1));
+        System.out.println("Final position of player1="+player1);
+        System.out.println("Final position of player2="+player2);
+        if(player1==100) {
+            System.out.println("Player1 is the winner!!");
+        }else if(player2==100){
+            System.out.println("Player2 is the winner!!");
+        }
+    }
+    public static void main(String[] args) {
+        dieRollPosition();
     }
 }
 
